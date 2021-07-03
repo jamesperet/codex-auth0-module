330f622999aeeecbf82dafe3d83337e9cdeed252
James Peret <james.peret@gmail.com>
2021-07-02 22:04:49 -0300

    Initial commit

---
+++ auth0.js
@@ -0,0 +1,13 @@
+class Auth0 {
+    module_name = "Auth0 Module";
+
+    constructor(program, config){
+        this.program = program;
+        this.config = config;
+    }
+
+    init(){}  
+    run(){}
+}
+
+module.exports = Auth0;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Codex Auth0 Module
+
+This module adds [Auth0](https://auth0.com/) support to the [jamesperet/codex-server](https://github.com/jamesperet/codex-server).
