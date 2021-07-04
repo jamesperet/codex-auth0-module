b404ea6f1801117004a56e49f871eb6cbb0d778f
James Peret <james.peret@gmail.com>
2021-07-03 21:15:16 -0300

    Added express-openid-connect

--- auth0.js
+++ auth0.js
@@ -1,12 +1,27 @@
-class Auth0 {
-    module_name = "Auth0 Module";
+const { auth } = require('express-openid-connect');
 
-    constructor(program, config){
-        this.program = program;
+class Auth0 {
+    
+    constructor(server, config){
+        this.server = server;
         this.config = config;
+        this.module_name = "Auth0 Module";
+        this.auth = auth;
     }
 
-    init(){}  
+    init()
+    {
+        this.server.express.use(
+            this.auth({
+              issuerBaseURL: this.config.data.issuer_base_url,
+              baseURL: this.config.data.base_url,
+              clientID: this.config.data.client_id,
+              secret: this.config.data.secret,
+              idpLogout: true,
+            })
+          );
+    }  
+
     run(){}
 }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
 # Codex Auth0 Module
 
 This module adds [Auth0](https://auth0.com/) support to the [jamesperet/codex-server](https://github.com/jamesperet/codex-server).
+
+Example config:
+
+```json
+{
+    "modules" : [
+        {
+            "name" : "codex-auth0-module",
+            "module" : "../codex-auth0-module/auth0.js",
+            "data"   : {
+                "issuer_base_url" : "https://dev-c28uur5g.us.auth0.com",
+                "client_id"       : "i5Ab6ixW0K3RlKpxcZ3KXQtKwZQRqROh",
+                "base_url"        : "http://localhost:3000",
+                "secret"          : "dLU1sLC_-mq9ZFWRFMPLDomNrZo02xycmB2hbSUPCcztFTW3Dshb2GcqFOEwdWFo"
+            }
+        }
+    ]
+}
+```
